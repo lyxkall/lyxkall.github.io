347eda65edff1e0e640f68e8f39feb0fb4805652
lyxkall <tatangmc3@gmail.com>
2024-10-17 19:12:24 +0700

    new layout page

--- script.js
+++ script.js
@@ -2,8 +2,4 @@ window.onscroll = () => {
 let header = document.querySelector('.header');
 
 header.classList.toggle('sticky', window.scrollY > 100);
-<<<<<<< HEAD
 };
-=======
-};
->>>>>>> 86dbb102a88422711f43b251df08638160f4fc3e
