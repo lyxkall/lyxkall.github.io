0dd459c8861c206d1411b0ce794df9abab9ebd61
lyxkall <tatangmc3@gmail.com>
2025-01-04 00:55:43 +0700

    Finla Version

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+// *========== scroll sections active link ==========*/
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle('bx-x');
+  navbar.classList.toggle('active');
+};
+
+// *========== scroll sections active link ==========*/
 let sections = document.querySelectorAll('section'); // Perbaikan selector
 let navLinks = document.querySelectorAll('.navbar a'); // Perbaikan selector
 
@@ -20,10 +30,16 @@ window.onscroll = () => {
     }
   });
 
-  // Sticky header
+  // Sticky navbar */
   let header = document.querySelector('.header');
+
   header.classList.toggle('sticky', window.scrollY > 100);
+
+/*========== remove menu icon navbar when click navbar link (scroll) =========*/
+menuIcon.classList.remove('bx-x');
+navbar.classList.remove('active');
 };
+
 
 /*========== Swiper Configuration =========*/
 var swiper = new Swiper(".mySwiper", {
